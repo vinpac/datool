44c52a727ed9e8e9e29bbf7b45efc0732a868370
Vin Pacheco <vin175pacheco@gmail.com>
2026-04-03 01:39:29 -0300

    fix build

--- components/ui/datool/core/search-bar.tsx
+++ components/ui/datool/core/search-bar.tsx
@@ -7,10 +7,12 @@ export function SearchBar({
   className,
   placeholder = "Search data...",
   query,
+  showLoadingOnRefetch = true,
 }: {
   className?: string
   placeholder?: string
   query?: string
+  showLoadingOnRefetch?: boolean
 }) {
   const datoolQuery = useDatoolSearch(query)
   const search = datoolQuery.search
@@ -19,11 +21,15 @@ export function SearchBar({
     return null
   }
 
+  const isLoading = showLoadingOnRefetch
+    ? datoolQuery.result.isFetching
+    : datoolQuery.result.isLoading
+
   return (
     <ConnectedSearchBar
       className={className}
       fields={datoolQuery.searchFields}
-      isLoading={datoolQuery.result.isFetching}
+      isLoading={isLoading}
       onSearchChange={search.onChange}
       placeholder={placeholder}
       value={search.value}
